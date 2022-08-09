112e3d1f31748c166e1e5ab0a905cd6ee55f0660
Bhupendra Yadav <54168804+bhupendra8753@users.noreply.github.com>
2022-08-09 15:52:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,34 +11,14 @@ app.use(urlencoded({ extended: false }));
 // It will be requested without a "Digits" parameter intitially, but subsequent
 // requests will contain "Digits"
 app.post('/voice', (request, response) => {
-  const { Digits, From } = request.body;
+  let twiml = `
+                <Response>
+                  <Dial>+12025550136</Dial>
+                </Response>
+                `;
   
-  console.log('Incoming call from ', From);
-  let twiml = '';
+  // Insert magic here
 
-  // This is the first time the URL has been requested, so no Digits
-  if (!Digits) {
-    twiml = `
-      <Response>
-        <Gather>
-          <Say>
-            Press any series of numbers on your keypad. Go nuts. You can end
-            your D T M F rampage by pressing the hash key.
-          </Say>
-        </Gather>
-      </Response>
-    `;
-  } else {
-    // If Digits has been populated, repeat them back
-    twiml = `
-      <Response>
-        <Say>You entered: ${Digits}</Say>
-        <Say>Goodbye!</Say>
-      </Response>
-    `;
-  }
-
-  // Finally, return the TwiML
   response.type('text/xml');
   response.send(twiml);
 });
